bab249250aadbac9b4626f7caf803cc7688e5077
TheB34ST <beast@TheB34STs-MacBook-Pro.local>
2023-07-27 16:16:28 -0500

    Fixed git issues

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/beast/Desktop/cosc/SoftwareDesignProject/app/layout.tsx": [
+      "/Users/beast/Desktop/cosc/SoftwareDesignProject/styles/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/beast/Desktop/cosc/SoftwareDesignProject/app/page": [
+      "/Users/beast/Desktop/cosc/SoftwareDesignProject/styles/globals.css"
+    ]
+  }
+}
